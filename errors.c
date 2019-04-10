9b7fb45f4e05e33fb4f090a9b4890b4517224260
WilliamASumner <williamasumner@gmail.com>
2019-04-10 11:58:09 -0400

    Added lexical error handling

--- errors.c
+++ errors.c
@@ -13,16 +13,33 @@ void yyerror(const char* s) { // what to do on an error in parser
     exit(1);
 }
 
-void print_semantic_error(enum error_type e) {
-    fprintf(stderr,"error: ");
+void print_lexical_error(enum lexical_error_type e, char* s)
+{
+    fprintf(stderr,"lexical error: ");
+    switch (e) {
+        case INTEGER_OVERFLOW:
+            fprintf(stderr,"Too large integer input");
+            break;
+        case UNRECOGNIZED_TOKEN:
+            fprintf(stderr,"Unrecognized token %s",s);
+            break;
+        default:
+            fprintf(stderr,"undefined ast error type encountered\n");
+            return;
+    }
+    printf("\n");
+}
+
+void print_semantic_error(enum semantic_error_type e,struct ast_node* node) {
+    fprintf(stderr,"semantic error: ");
     switch (e) {
         case USE_BEFORE_DEC:
             fprintf(stderr,"Use of variable or function before declaration");
             break;
-        case LAST_DEC_NOT_MAIN:
+        case LAST_DEC_NOT_VALID_MAIN:
             fprintf(stderr,"Function \"void main(void)\" is not last declaration");
             break;
-        case REDECLARATION:
+        case REDECLARATION_OR_BAD_SCOPE:
             fprintf(stderr,"Redeclaration of variable or function");
             break;
         case PARAMS_MISMATCH:
@@ -31,33 +48,113 @@ void print_semantic_error(enum error_type e) {
         case INVALID_ARR_ACCESS:
             fprintf(stderr,"Invalid array access");
             break;
-        case INCORRECT_TYPE_ASSG:
-            fprintf(stderr,"Incorrect type assignment");
+        case TYPE_MISMATCH:
+            fprintf(stderr,"Mismatch of types");
             break;
-        case IMPROPER_RET_TYPE:
-            fprintf(stderr,"Mismatched return type");
+        case CALL_MISMATCH:
+            fprintf(stderr,"Mismatch of call to function");
             break;
-        case IMPROPER_EXPR_TYPE:
-            fprintf(stderr,"Improper expression type");
+        case ID_TYPE_MISMATCH:
+            fprintf(stderr,"Mismatch of id types");
+            break;
+        case NON_INT_VAR:
+            fprintf(stderr,"Non-integer type of variable");
+            break;
+        case NON_INT_SMP_EXPR:
+            fprintf(stderr,"Non-integer type of simple-expression");
+            break;
+        case NON_INT_ADD_EXPR:
+            fprintf(stderr,"Non-integer type of addition-expression");
+            break;
+        case NON_INT_TERM:
+            fprintf(stderr,"Non-integer type of term");
+            break;
+        case NON_INT_FACT:
+            fprintf(stderr,"Non-integer type of factor");
+            break;
+        case BAD_RETURN_TYPE:
+            fprintf(stderr,"Bad return type");
+            break;
+        case MISSING_RETURN:
+            fprintf(stderr,"Missing return in function");
             break;
         case ALL_GOOD:
             fprintf(stderr,"All is good");
             break;
         default:
-            fprintf(stderr,"undefined error type encountered\n");
+            fprintf(stderr,"undefined semantic error type encountered\n");
             return;
     }
-    printf("\n");
+    printf(" at node %p\n",node);
 }
 
-void throw_semantic_error(enum error_type err) {
+void print_ast_error(enum ast_error_type e, struct ast_node* node)
+{
+    fprintf(stderr,"ast error: ");
+    switch (e) {
+        case EMPTY_VAR_DEC:
+            fprintf(stderr,"Empty variable declaration in AST");
+            break;
+        case EMPTY_FUNC_DEC:
+            fprintf(stderr,"Empty function declaration in AST");
+            break;
+        case EMPTY_DECLIST:
+            fprintf(stderr,"Empty declaration list in AST");
+            break;
+        case VAR_AND_FUNC_DEC:
+            fprintf(stderr,"Function and Variable declaration in AST");
+            break;
+        case TYPE_CHECK_ON_NULL_NODE:
+            fprintf(stderr,"Type check performed on null node in AST");
+            break;
+        case INVALID_EXPR_TYPE:
+            fprintf(stderr,"Invalid expr_type found in AST");
+            break;
+        case IMPROPER_NODE_TYPE:
+            fprintf(stderr,"Improper node_type in AST");
+            break;
+        case INVALID_FACTOR_TYPE:
+            fprintf(stderr,"Improper factor_type in AST");
+            break;
+        case INCOMPLETE_EXPR_NODE:
+            fprintf(stderr,"Incomplete expression node found in AST");
+            break;
+        case INCOMPLETE_SELE_NODE:
+            fprintf(stderr,"Incomplete selection node found in AST");
+            break;
+        case INCOMPLETE_ITER_NODE:
+            fprintf(stderr,"Incomplete iteration node found in AST");
+            break;
+        default:
+            fprintf(stderr,"undefined ast error type encountered\n");
+            return;
+    }
+    printf(" at node %p\n",node);
+}
+
+void throw_lexical_error(enum lexical_error_type err,char* s) {
 #ifdef DEBUG
-    print_semantic_error(err); // show what error we got
+    print_lexical_error(err,s); // show what error we got
+#endif
+    cleanup(); // clean up table and ast if need be
+    exit(1); // and exit
+}
+
+void throw_semantic_error(enum semantic_error_type err,struct ast_node* node) {
+#ifdef DEBUG
+    print_semantic_error(err,node); // show what error we got
 #endif
     cleanup(); // clean up table and ast
     exit(1); // and exit
 }
 
+void throw_ast_error(enum ast_error_type err, struct ast_node* node) {
+#ifdef DEBUG
+    print_ast_error(err,node); // show what error we got
+#endif
+    cleanup(); // clean up table and ast
+    exit(1); // and exit
+}
 
 void cleanup() {
     free_ast_tree(ast); // free the ast
--- errors.h
+++ errors.h
@@ -4,20 +4,48 @@
 extern struct ast_node* ast;
 extern struct sym_table* table;
 
-enum error_type {
+enum lexical_error_type {
+    UNRECOGNIZED_TOKEN,
+    INTEGER_OVERFLOW
+};
+
+enum semantic_error_type {
     USE_BEFORE_DEC,
-    LAST_DEC_NOT_MAIN,
-    REDECLARATION,
+    LAST_DEC_NOT_VALID_MAIN,
+    REDECLARATION_OR_BAD_SCOPE,
     PARAMS_MISMATCH,
     INVALID_ARR_ACCESS,
-    INCORRECT_TYPE_ASSG,
-    IMPROPER_RET_TYPE,
-    IMPROPER_EXPR_TYPE,
+    TYPE_MISMATCH,
+    CALL_MISMATCH,
+    ID_TYPE_MISMATCH,
+    NON_INT_VAR,
+    NON_INT_SMP_EXPR,
+    NON_INT_ADD_EXPR,
+    NON_INT_TERM,
+    NON_INT_FACT,
+    BAD_RETURN_TYPE,
+    MISSING_RETURN,
     ALL_GOOD
 };
 
+enum ast_error_type {
+    EMPTY_VAR_DEC,
+    EMPTY_FUNC_DEC,
+    EMPTY_DECLIST,
+    VAR_AND_FUNC_DEC,
+    TYPE_CHECK_ON_NULL_NODE,
+    INVALID_EXPR_TYPE,
+    INVALID_FACTOR_TYPE,
+    IMPROPER_NODE_TYPE,
+    INCOMPLETE_EXPR_NODE,
+    INCOMPLETE_SELE_NODE,
+    INCOMPLETE_ITER_NODE
+};
+
 void yyerror(const char* s);
-void print_parser_error(enum error_type e);
+void throw_lexical_error(enum lexical_error_type e,char* s);
+void throw_semantic_error(enum semantic_error_type e,struct ast_node* node);
+void throw_ast_error(enum ast_error_type e, struct ast_node* node);
 void cleanup();
 
 #endif
