5f75f1055731465ff32262a8ca7abd9bc2f0227e
WilliamASumner <williamasumner@gmail.com>
2019-04-20 18:55:52 -0400

    added more memory to be displayed

--- mrun.sh
+++ mrun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 1 ]; then
-    java -jar mars.jar $s0 $s1 0x10010000-0x10010010 $1
+    java -jar mars.jar $s0 $s1 0x10010000-0x10010040 $1
 else
     java -jar mars.jar 0x10010000-0x10010010 output.asm
 fi
