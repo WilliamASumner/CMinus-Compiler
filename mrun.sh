594f1747f254d136f49e704a40b4d63e88c2ac18
WilliamASumner <williamasumner@gmail.com>
2019-04-23 16:52:14 -0400

    updated mem values

--- mrun.sh
+++ mrun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+#$sp, $ra print values of these bad boois
+#$1 - filename
 if [ $# -eq 1 ]; then
-    java -jar mars.jar $s0 $s1 0x10010000-0x10010040 $1
+    java -jar mars.jar $sp $ra 0x10010000-0x10010040 $1
 else
     java -jar mars.jar 0x10010000-0x10010010 output.asm
 fi
