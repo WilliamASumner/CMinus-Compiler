304a7aa95f95a3cb972770e9fb18335cb8d72147
WilliamASumner <williamasumner@gmail.com>
2019-04-23 16:53:25 -0400

    debug now runs automatically

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 if [[ -f "./yaccdebug.out" ]]; then
-    lldb ./yaccdebug.out $1 $2
+    if [ $# -eq 2 ]; then
+        lldb -s .lldbstart -f ./yaccdebug.out $1 $2
+    else
+        file=$(basename $1) # get file
+        lldb -s .lldbstart -f ./yaccdebug.out $1 ${file%.*}.asm
+    fi
+
 elif [[ -f "./yaccprog.out" ]]; then
     ./yaccprog.out $1 $2
 fi
