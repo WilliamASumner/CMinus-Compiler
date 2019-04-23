83fa5e3452971e982de2f9828dee6b5ac9c9a0a2
WilliamASumner <williamasumner@gmail.com>
2019-04-23 16:52:58 -0400

    added stack

--- prep.sh
+++ prep.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-tar -cvf parser.tar Makefile \
+tar -cvf parser.tar oldmakefile \
     run.sh compile.sh \
     parser.y lexer.l \
-    ast.*  \
-    symtable.* \
-    errors.* \
+    ast.{c,h}  \
+    symtable.{c,h} \
+    errors.{c,h} \
     id.h \
     yacc_header.h \
-    codegen.{c,h}
+    codegen.{c,h} \
+    stack.{c,h}
