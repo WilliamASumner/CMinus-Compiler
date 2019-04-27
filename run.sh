188edc6fb41d62a83070d86d120ae0cb79cbde8f
WilliamASumner <williamasumner@gmail.com>
2019-04-27 16:17:14 -0400

    check whether correct number of args

--- run.sh
+++ run.sh
@@ -2,11 +2,21 @@
 if [[ -f "./cminusdebug" ]]; then
     if [ $# -eq 2 ]; then
         lldb -s .lldbstart -f ./cminusdebug $1 $2
-    else
+    elif [ $# -eq 1 ]; then
         file=$(basename $1) # get file
         lldb -s .lldbstart -f ./cminusdebug $1 ${file%.*}.asm
+    else
+        echo "invalid number of args"
     fi
 
 elif [[ -f "./cminus" ]]; then
-    ./cminus $1 $2
+    if [ $# -eq 2 ]; then
+        ./cminus $1 $2
+    elif [ $# -eq 1 ]; then
+        ./cminus $1
+    else
+        echo "invalid number of args"
+    fi
+else
+    echo "no compiler binary found"
 fi
