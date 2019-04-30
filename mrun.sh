af34b97ec8848c172d61122fc59641cc91751585
WilliamASumner <williamasumner@gmail.com>
2019-04-29 20:56:57 -0400

    changed output from running an assembly file

--- mrun.sh
+++ mrun.sh
@@ -2,7 +2,7 @@
 #$sp, $ra print values of these bad boois
 #$1 - filename
 if [ $# -eq 1 ]; then
-    java -jar mars.jar $sp $ra 0x10010000-0x10010040 $1
+    java -jar mars.jar $1
 else
     java -jar mars.jar 0x10010000-0x10010010 output.asm
 fi
